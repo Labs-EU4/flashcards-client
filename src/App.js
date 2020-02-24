84715afdd21b978186e1f0bd2cf97d5878e4e579
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-02-24 19:52:46 +0500

    (bugfix)Fixed store import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import AppRouter from "./router";
 import { Provider } from "react-redux";
-import { store } from "./state/store";
+import store from "./state/store";
 
 const App = () => {
   return (
