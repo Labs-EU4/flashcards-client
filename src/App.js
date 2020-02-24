66299f93e22aa48db80314fad92b576cf346794c
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-02-24 10:31:15 +0100

    installed and imported antd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import AppRouter from './router';
-
+import React from "react";
+import AppRouter from "./router";
+import "./App.css";
 
 const App = () => {
-  return (
-    <AppRouter />
-  );
-}
+  return <AppRouter />;
+};
 
 export default App;
