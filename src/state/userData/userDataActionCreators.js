49471605d2bbcc679301b64959ea0a92e63772e1
Darragh Ferry <darraghferry@hotmail.com>
2020-02-24 17:12:03 +0000

    successful register now also logs you in

--- src/state/userData/userDataActionCreators.js
+++ src/state/userData/userDataActionCreators.js
@@ -1,15 +1,22 @@
-import axios from 'axios';
-import * as types from './userDataActionTypes';
+import axios from "axios";
+import * as types from "./userDataActionTypes";
 
-export const registerNewUser = (newUser) => dispatch => {
-    axios.post("http://localhost:4003/api/auth/register", newUser)
+//creates a new user and if successful, logs them in and sets token in local storage
+export const registerNewUser = newUser => dispatch => {
+  axios
+    .post("http://localhost:4003/api/auth/register", newUser)
     .then(res => {
-        console.log(res.data.data.user);
+      delete newUser.fullName;
+      axios.post("http://localhost:4003/api/auth/login", newUser).then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.data.token);
         dispatch({
-            type: types.UPDATE_USER_DETAILS,
-            payload: {userData: res.data.data.user}
-        })
-    }).catch(err => {
-        console.log(err)
+          type: types.UPDATE_USER_DETAILS,
+          payload: { userData: res.data.data.user }
+        });
+      });
     })
-}
+    .catch(err => {
+      console.log(err);
+    });
+};
