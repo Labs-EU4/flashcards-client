e688b777f6fac20e64b68f423ecf8f3cfb12f64b
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-02-24 16:41:28 +0100

    conflicts solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import AppRouter from "./router";
-import Provider from "react-redux";
+import {Provider} from "react-redux";
 import store from "./state/store";
 
 const App = () => {
