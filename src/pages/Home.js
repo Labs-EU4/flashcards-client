43e2ae11a051e162f44abcfea450b7fed55d8389
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-02-24 12:13:00 +0100

    added less and and other dependencies to be able to use all features from ant-design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BasicLayout from '../layout/BasicLayout';
+import { Button } from 'antd';
 
 const Home = () => {
   return (
     <BasicLayout>
       <h1>This is Home!</h1>
+      <Button type="primary">Button</Button>
     </BasicLayout>
   )
 }
