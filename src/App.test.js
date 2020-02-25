e0edc55f262132d24859b70951c7ed259d786ad4
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-02-25 12:41:26 +0500

    (bugfix) typo fix in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as rtl from '@testing-library/React';
+import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App.js'
 
