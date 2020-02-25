99e85b6325dc312968b2290ecc34fe313fc4db0f
Darragh Ferry <darraghferry@hotmail.com>
2020-02-25 14:36:03 +0000

    fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import AppRouter from "./router";
-import { Provider } from "react-redux";
-import store from "./state/store";
 import RegisterForm from "./components/RegisterForm";
-import { PrivateRoute } from "./router/PrivateRoute";
-import { Route, withRouter } from "react-router-dom";
+import {PrivateRoute} from "./router/PrivateRoute";
+import {Route, withRouter} from "react-router-dom";
 import "./CSS/index.css";
 
 const App = () => {
   return (
-    <Provider store={store}>
+    <>
       <PrivateRoute exact path="/" component={AppRouter} />
       <Route path="/register" component={RegisterForm} />
-    </Provider>
+    </>
   );
 };
 
