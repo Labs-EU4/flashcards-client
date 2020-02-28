f40150a3df522166beb6614069c3d19828a72a11
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-02-28 15:55:26 +0500

    RoutesConfig comments and callback factory

--- src/router/RoutesConfig.js
+++ src/router/RoutesConfig.js
@@ -2,29 +2,38 @@ import Home from "../pages/Home";
 import PageNotFound from "../pages/404";
 import Login from "../pages/Login";
 import Dashboard from "../components/dashboard";
-import checkLoginState from "./checkLoginState";
 import {Redirect} from "react-router-dom";
 import React from "react";
-
+//This function is connected directly to the store and checks if user is logged in or not.
+import checkLoginState from "./checkLoginState";
 /*
   Routes config must be ordered the same way you'd 
   do inside a `<Switch>`. The last route object is 
   a "fallback" route, to catch 404 errors.
 */
 
+function createRenderCallback(Component) {
+  //Creates a render callback for protected pages.
+  return function(props) {
+    return checkLoginState() ? <Component /> : <Redirect to="/login" />;
+  };
+}
+
 const RoutesConfig = [
   {
     path: "/",
-    render: props => (checkLoginState() ? <Home /> : <Redirect to="/login" />),
+    //protected route takes path and a render prop
+    render: createRenderCallback(Home),
   },
 
   {
     path: "/login",
+    //Unprotected route takes path and a component prop
     component: Login,
   },
   {
     path: "/dashboard",
-    render: props => (checkLoginState() ? <Dashboard /> : <Redirect to="/login" />),
+    render: createRenderCallback(Dashboard),
   },
 
   {
