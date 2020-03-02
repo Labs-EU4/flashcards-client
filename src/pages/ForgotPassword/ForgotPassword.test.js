1f94ae0c8e47401aaf3e5e61f36d138ec68e2a94
Niklas Becker <48069565+niklasbec@users.noreply.github.com>
2020-03-02 15:17:47 +0100

    added more tests

--- src/pages/ForgotPassword/ForgotPassword.test.js
+++ src/pages/ForgotPassword/ForgotPassword.test.js
@@ -3,6 +3,7 @@ import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import FormComponent from "../../components/ForgotPassword/FormComponent";
 
+// cleaning up
 afterEach(rtl.cleanup);
 
 let wrapper;
