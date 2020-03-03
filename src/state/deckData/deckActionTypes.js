e3426d479a8a68f826f985ce4ddbb95fdf073027
maxjamb <maxwelljamesbeard@gmail.com>
2020-03-03 12:05:27 +0000

    built intitial state slices, action types, reducers, added to monster reducer

---
+++ src/state/deckData/deckActionTypes.js
@@ -0,0 +1,4 @@
+export const CREATE_DECK = "CREATE_DECK";
+export const READ_DECK = "READ_DECK";
+export const UPDATE_DECK = "UPDATE_DECK";
+export const DELETE_DECK = "DELETE_DECK";
---
+++ src/state/deckData/deckReducers.js
@@ -0,0 +1,18 @@
+import * as types from "./deckActionTypes";
+
+const initialState = {};
+
+export function decksReducer(decks = initialState.decks, action) {
+  switch (action.type) {
+    case types.CREATE_DECK:
+      return action.payload.deck;
+    case types.READ_DECK:
+      return decks;
+    case types.UPDATE_DECK:
+      return decks;
+    case types.DELETE_DECK:
+      return decks.filter(deck => deck.id !== action.payload.id);
+    default:
+      return decks;
+  }
+}
