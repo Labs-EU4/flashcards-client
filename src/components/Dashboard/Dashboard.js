726cebbbf2df99be7b7088ac6d0851b45ee8b723
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-03-04 11:54:04 +0100

    added section for content and fitted the styling

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -11,18 +11,7 @@ const Dashboard = props => {
 
   if (collapsed === true) {
     return (
-      <div className={styles.toggleIcon}>
-        <Icon
-          type="menu"
-          style={{fontSize: "30px"}}
-          theme="outlined"
-          onClick={() => toggle()}
-        />
-      </div>
-    );
-  } else {
-    return (
-      <div className={styles.dashboardLeft}>
+      <>
         <div className={styles.toggleIcon}>
           <Icon
             type="menu"
@@ -31,39 +20,57 @@ const Dashboard = props => {
             onClick={() => toggle()}
           />
         </div>
-        <h1 className={styles.logo}>Logo</h1>
-        <h3 className={styles.greeting}>Welcome Username!</h3>
-        <div className={styles.menu}>
-          <Menu
-            style={{width: 270}}
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-          >
-            <Menu.Item key="1">
-              <Icon type="home" />
-              Home
-            </Menu.Item>
 
-            <Menu.Item key="2">
-              {" "}
-              <Icon type="block" />
-              Deck Library
-            </Menu.Item>
-            <Menu.Item key="3">
-              {" "}
-              <Icon type="global" />
-              Discover Decks
-            </Menu.Item>
-          </Menu>
+        <div className={styles.dashboardRight}>content</div>
+      </>
+    );
+  } else {
+    return (
+      <div className={styles.dashboard}>
+        <div className={styles.dashboardLeft}>
+          <div className={styles.toggleIcon}>
+            <Icon
+              type="menu"
+              style={{fontSize: "30px"}}
+              theme="outlined"
+              onClick={() => toggle()}
+            />
+          </div>
+          <h1 className={styles.logo}>Logo</h1>
+          <h3 className={styles.greeting}>Welcome Username!</h3>
+          <div className={styles.menuContainer}>
+            <Menu
+              className={styles.menu}
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+            >
+              <Menu.Item key="1">
+                <Icon type="home" />
+                Home
+              </Menu.Item>
+
+              <Menu.Item key="2">
+                {" "}
+                <Icon type="block" />
+                Deck Library
+              </Menu.Item>
+              <Menu.Item key="3">
+                {" "}
+                <Icon type="global" />
+                Discover Decks
+              </Menu.Item>
+            </Menu>
+          </div>
+          <Button type="primary" className={styles.logoutButton}>
+            Logout
+          </Button>
+          <footer className={styles.footer}>
+            <p>
+              <Icon type="setting" /> Profile Settings
+            </p>
+          </footer>
         </div>
-        <Button type="primary" className={styles.logoutButton}>
-          Logout
-        </Button>
-        <footer className={styles.footer}>
-          <p>
-            <Icon type="setting" /> Profile Settings
-          </p>
-        </footer>
+        <div className={styles.dashboardRight}>content</div>
       </div>
     );
   }
