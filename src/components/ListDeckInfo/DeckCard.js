1c94eeaafbcb2f0c325a8b13bbf2b25345c5e8c6
Darragh Ferry <darraghferry@hotmail.com>
2020-03-05 14:36:27 +0000

    style changes

--- src/components/ListDeckInfo/DeckCard.js
+++ src/components/ListDeckInfo/DeckCard.js
@@ -6,17 +6,21 @@ export default function DeckCard({deck}) {
   return (
     <div className={styles.deckCard}>
       <div className={styles.deckOverview}>
-        <div>
+        <div className={styles.header}>
           <h2>{deck.deck_name}</h2>
-          <h3>Author: {deck.author}</h3>
-          <h3>No. of Cards: {deck.flashcards.length}</h3>
+          <div className={styles.info}>
+            <h3 className={styles.info_headings}>Author: {deck.author}</h3>
+            <h3 className={styles.info_headings}>
+              No. of Cards: {deck.flashcards.length}
+            </h3>
+          </div>
         </div>
         <div>
           <Icon type="like" style={{color: "rgba(0,0,0,.25)"}} />
           <Icon type="dislike" style={{color: "rgba(0,0,0,.25)"}} />
         </div>
       </div>
-      <div className={styles.horizontalList}>
+      {/* <div className={styles.horizontalList}>
         {deck.flashcards.map(card => {
           return (
             <Card title="Question" className={styles.card}>
@@ -24,7 +28,7 @@ export default function DeckCard({deck}) {
             </Card>
           );
         })}
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/pages/PublicDecks/PublicDecks.js
+++ src/pages/PublicDecks/PublicDecks.js
@@ -4,9 +4,9 @@ import Header from "../../components/ListDeckInfo/HeaderSearchBar";
 
 export default function PublicDecks() {
   return (
-    <>
+    <div style={{backgroundColor: "gray"}}>
       <Header />
       <List />
-    </>
+    </div>
   );
 }
