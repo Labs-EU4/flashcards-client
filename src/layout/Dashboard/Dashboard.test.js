883c66a529b42c352760757f53e69a31891aa93c
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-03-06 16:00:56 +0100

    try to pass travis checks

--- src/layout/Dashboard/Dashboard.test.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import {BrowserRouter} from "react-router-dom";
-import * as rtl from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import {Dashboard} from "./Dashboard";
