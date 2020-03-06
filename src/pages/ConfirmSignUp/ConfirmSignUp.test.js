a09e519e620312cc7f0729f342eeef450bb938c1
Niklas Becker <48069565+niklasbec@users.noreply.github.com>
2020-03-06 11:30:05 +0100

    changing the logo in header

--- src/pages/ConfirmSignUp/ConfirmSignUp.test.js
+++ src/pages/ConfirmSignUp/ConfirmSignUp.test.js
@@ -2,9 +2,9 @@ import React from "react";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import ConfirmSignUp from "./ConfirmSignUp";
-
+import {BrowserRouter} from "react-router-dom";
 // cleaning up
-afterEach(rtl.cleanup);
+// afterEach(rtl.cleanup);
 
 // useHistory mock so tests dont crash
 
@@ -20,8 +20,30 @@ jest.mock("react-router-dom", () => ({
 let wrapper;
 
 describe("is the component rendering correctly", () => {
+  wrapper = rtl.render(
+    <BrowserRouter>
+      <ConfirmSignUp />
+    </BrowserRouter>
+  );
   it("dummy test", () => {
     let queryValue = "hello";
     expect(queryValue).toBeDefined();
+  });
+});
+
+describe("error tests", () => {
+  wrapper = rtl.render(
+    <BrowserRouter>
+      <ConfirmSignUp />
+    </BrowserRouter>
+  );
+  jest.mock("axios", () => {
+    return {
+      post: jest.fn().mockImplementationOnce(() => Promise.resolve("token")),
+    };
+  });
+  it("it returns an error if token is invalid", () => {
+    let queryValue = wrapper.getByTestId("alertSuccess");
+    expect(queryValue).toBeVisible();
   });
 });
