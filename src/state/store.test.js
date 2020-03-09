7ac5680611633a54c8dbbc08e1a7b784245721f7
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-03-09 18:44:14 +0500

    Action tests

---
+++ src/state/store.test.js
@@ -0,0 +1,124 @@
+import * as actions from "./actions/auth";
+import * as types from "./types/index";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import * as axiosMock from "../utils/axios";
+
+jest.mock("../utils/axios");
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe("Auth actions", () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  test("Login action dispatches LOGIN_START and LOGIN_SUCCESS on resolve from backend", () => {
+    //set axios moxk to resolve once
+    axiosMock.justAxios.post = jest
+      .fn()
+      .mockResolvedValueOnce({data: {data: {user: "HEY", token: "BABY"}}});
+    //mock store in initial state
+    const store = mockStore({
+      auth: {
+        isLoggedIn: false,
+        user: {},
+        token: null,
+      },
+    });
+    const expectedActions = [
+      {type: types.LOGIN_START},
+      {type: types.LOGIN_SUCCESS, payload: {user: "HEY", token: "BABY"}},
+    ];
+    return store
+      .dispatch(actions.login({username: "BABY", password: "OOOH"}))
+      .then(() => {
+        // return of async actions
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(axiosMock.justAxios.post).toHaveBeenCalledWith("/auth/login", {
+          username: "BABY",
+          password: "OOOH",
+        });
+      });
+  });
+  test("Login action throws on backend error", async () => {
+    //Arrange
+    //set axios mock to reject once
+    axiosMock.justAxios.post = jest.fn().mockRejectedValueOnce({message: "ERROR!!!"});
+    //Act & Assert
+    await expect(actions.login()(jest.fn())).rejects.toThrowError("ERROR!!!");
+  });
+  test("registerNewUser action dispatches REGISTER SUCCESS on succesfull request", () => {
+    //set axios mock to resolve once
+    axiosMock.justAxios.post = jest
+      .fn()
+      .mockResolvedValueOnce({data: {data: {user: "HEY", token: "BABY"}}});
+    //mock store in initial state
+    const store = mockStore();
+    const expectedActions = [
+      {type: types.REGISTER_SUCCESS, payload: {user: "HEY", token: "BABY"}},
+    ];
+    return store
+      .dispatch(actions.registerNewUser({username: "BABY", password: "OOOH"}))
+      .then(() => {
+        // return of async actions
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(axiosMock.justAxios.post).toHaveBeenCalledWith("/auth/register", {
+          username: "BABY",
+          password: "OOOH",
+        });
+      });
+  });
+  test("addRecoveryPassword action dispatches LOGIN_SUCCESS on succesfull request", () => {
+    //Arrange
+    //set axios mock to resolve once
+    axiosMock.justAxios.post = jest
+      .fn()
+      .mockResolvedValueOnce({data: {data: {user: "HEY", token: "BABY"}}});
+    //mock store in initial state
+    const store = mockStore();
+    const expectedActions = [
+      {type: types.LOGIN_SUCCESS, payload: {user: "HEY", token: "BABY"}},
+    ];
+    //ACT
+    return store.dispatch(actions.addRecoveryPassword("BABYYY", "TOKEN")).then(() => {
+      // return of async actions
+      //ASSERT
+      expect(store.getActions()).toEqual(expectedActions);
+      expect(axiosMock.justAxios.post).toHaveBeenCalledWith(
+        "/auth/recovery",
+        {password: "BABYYY"},
+        {
+          headers: {Authorization: "TOKEN"},
+        }
+      );
+    });
+  });
+  test("googleAuthorized action dispatches LOGIN_START & LOGIN_SUCCESS on succesfull request", () => {
+    //Arrange
+    //set axios mock to resolve once
+    let postMock = jest
+      .fn()
+      .mockResolvedValueOnce({data: {data: {user: "HEY", token: "BABY"}}});
+    axiosMock.axiosWithAuth = () => {
+      return {
+        post: postMock,
+      };
+    };
+    //mock store in initial state
+    const store = mockStore();
+    const expectedActions = [
+      {type: types.LOGIN_START},
+      {type: types.LOGIN_SUCCESS, payload: {user: "HEY", token: "BABY"}},
+    ];
+    //ACT
+    return store
+      .dispatch(actions.googleAuthorized("TOKEN", {push: jest.fn()}))
+      .then(() => {
+        // return of async actions
+        //ASSERT
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(postMock).toHaveBeenCalledWith("/auth/google/TOKEN");
+      });
+  });
+});
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,16 +1,12 @@
 import axios from "axios";
 export const baseURL =
   process.env.REACT_APP_API_HOST || "https://flashdecks-staging.herokuapp.com/api";
-export const justAxios = () => {
-  const instance = axios.create({
-    baseURL,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  return instance;
-};
+export const justAxios = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
