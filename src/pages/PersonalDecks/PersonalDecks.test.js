4d2a7711b1089c036ec3859aa08e566e7f9b9324
Darragh Ferry <darraghferry@hotmail.com>
2020-03-11 10:55:50 +0000

    updated PersonalDecks tests to pass with new redux implementation

--- src/pages/PersonalDecks/PersonalDecks.test.js
+++ src/pages/PersonalDecks/PersonalDecks.test.js
@@ -1,11 +1,17 @@
 import React from "react";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import PersonalDecks from "./PersonalDecks";
+import {PersonalDecks} from "./PersonalDecks";
+import {Provider} from "react-redux";
+import store from "../../state/store";
 
 let wrapper;
 beforeEach(() => {
-  wrapper = rtl.render(<PersonalDecks />);
+  wrapper = rtl.render(
+    <Provider store={store}>
+      <PersonalDecks />
+    </Provider>
+  );
 });
 
 it("renders without crashing", () => {
