45d71f464402e4bd7b7d5a7fa49a061834cd639e
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-03-18 09:31:19 +0100

    Changed key for the decklist

--- src/components/ListDeckInfo/DeckList.js
+++ src/components/ListDeckInfo/DeckList.js
@@ -6,8 +6,8 @@ export default function DecksList({decks}) {
   return (
     <div className={styles.main_content}>
       {decks.length > 0 ? (
-        decks.map((deck, index) => {
-          return <DeckCard deck={deck} key={index} />;
+        decks.map(deck => {
+          return <DeckCard deck={deck} key={deck.deck_id} />;
         })
       ) : (
         <h2 className={styles.noName}>You have no decks right now</h2>
