3dc347d1218be2abbec59891ace494044b33c11c
Babatunde <babatundea15@gmail.com>
2020-03-20 19:41:12 +0100

    fixed the issue of uneven card display

--- src/components/CreateCard/Cards.js
+++ src/components/CreateCard/Cards.js
@@ -48,72 +48,72 @@ function Cards(props) {
   };
 
   return (
-    //   Map the fetched cards to an ant design cards component for display on the browser
-
     <div className={styles.cardContainer}>
-      <Dashboard>
-        <div className={styles.cardDeck}>
-          <div className={styles.container}>
-            <div className={styles.title}>
-              <h2 className={styles.heading}>Deck name</h2>
-              <HeaderSearchBar />
-              <Button className={styles.btn} type="dashed" onClick={toggleMode}>
-                Add a card
-              </Button>
-            </div>
-            <Spin spinning={loading} delay={300}>
-              <Modal
-                title="Want a new card?"
-                visible={show}
-                footer={null}
-                onCancel={handleAdd}
-              >
-                <AddCard toggleMode={toggleMode} />
-              </Modal>
-            </Spin>
-            <div className={styles.mappedCard}>
-              {props.currentDeck.flashcards &&
-              props.currentDeck.flashcards.length !== 0 ? (
-                props.currentDeck.flashcards.map(currentCard => {
-                  return (
-                    <div className={styles.card}>
-                      <Card
-                        data-testid="cardHolder"
-                        style={{width: "100%", marginTop: 16}}
-                        className={styles.innerCard}
-                        actions={[
-                          <DeleteOutlined
-                            key="delete"
-                            onClick={() => handleDelete(currentCard.id)}
-                          />,
-                          <EditOutlined onClick={toggleModal} />,
-                        ]}
-                      >
-                        <Meta
-                          avatar={<Avatar src="logo192.png" />}
-                          title={currentCard.question}
-                          description={currentCard.answer}
-                        />
-                      </Card>
-                      <Modal
-                        title="Are you sure you want to edit this card?"
-                        visible={visible}
-                        footer={null}
-                        // onOk={handleOk}
-                        onCancel={handleCancel}
-                      >
-                        <UpdateCard toggleModal={toggleModal} cardId={currentCard.id} />
-                      </Modal>
-                    </div>
-                  );
-                })
-              ) : (
-                <h1>THERE ARE NO CARDS TO DISPLAY</h1>
-              )}
-            </div>
-          </div>
+      {/* <Dashboard/> */}
+      <div className={styles.header}>
+        <p className={styles.deckName}>Deck name</p>
+        <HeaderSearchBar />
+        <Button className={styles.btn} type="dashed" onClick={toggleMode}>
+          Add a card
+        </Button>
+      </div>
+      <div className={styles.mainContent}>
+        <Dashboard />
+        <div className={styles.mappedCard}>
+          {/* Map the fetched cards to an ant design cards component for display on the
+          browser */}
+          {props.currentDeck.flashcards && props.currentDeck.flashcards.length !== 0 ? (
+            props.currentDeck.flashcards.map(currentCard => {
+              return (
+                <div className={styles.card}>
+                  <Card
+                    data-testid="cardHolder"
+                    style={{width: "100%", marginTop: "16px", marginRight: "10px"}}
+                    className={styles.innerCard}
+                    actions={[
+                      <DeleteOutlined
+                        key="delete"
+                        onClick={() => handleDelete(currentCard.id)}
+                      />,
+                      <EditOutlined onClick={toggleModal} />,
+                    ]}
+                  >
+                    <Meta
+                      avatar={<Avatar src="logo192.png" />}
+                      title={currentCard.question}
+                      description={currentCard.answer}
+                    />
+                  </Card>
+                  <Modal
+                    title="Are you sure you want to edit this card?"
+                    visible={visible}
+                    footer={null}
+                    // onOk={handleOk}
+                    onCancel={handleCancel}
+                  >
+                    <UpdateCard toggleModal={toggleModal} cardId={currentCard.id} />
+                  </Modal>
+                </div>
+              );
+            })
+          ) : (
+            <h1>THERE ARE NO CARDS TO DISPLAY</h1>
+          )}
         </div>
-      </Dashboard>
+      </div>
+      <div className={styles.cardDeck}>
+        {/* <div className={styles.container}> */}
+        {/* <div className={styles.title}>
+              
+            
+            </div> */}
+        {/* <Spin spinning={loading} delay={300}></Spin> */}
+        <Modal title="Want a new card?" visible={show} footer={null} onCancel={handleAdd}>
+          <AddCard toggleMode={toggleMode} />
+        </Modal>
+      </div>
+      {/* </div> */}
+      {/* </Dashboard> */}
     </div>
   );
 }
