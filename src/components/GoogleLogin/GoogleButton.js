3964899e54fde6e91ba20e7d6f15e5e2676b87cf
maxjamb <maxwelljamesbeard@gmail.com>
2020-03-23 15:36:26 +0000

    fixed register styles bug

--- src/components/GoogleLogin/GoogleButton.js
+++ src/components/GoogleLogin/GoogleButton.js
@@ -5,13 +5,11 @@ import {baseURL} from "../../utils/axios";
 export default function GoogleButton({children}) {
   return (
     <a className={styles.google_auth} href={`${baseURL}/auth/google`}>
-      <div className={styles.gstyle}>
-        <img
-          className={styles.googleIcon}
-          alt="google-icon"
-          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-        />
-      </div>
+      <img
+        className={styles.google_icon}
+        alt="google-icon"
+        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+      />
       {children}
     </a>
   );
