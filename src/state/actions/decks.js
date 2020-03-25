c6d68f7654e1c3fca3ec8a8c5eba60fce2eb2077
Darragh Ferry <darraghferry@hotmail.com>
2020-03-25 12:55:02 +0000

    new deck card now displays deck title

--- src/state/actions/decks.js
+++ src/state/actions/decks.js
@@ -93,13 +93,9 @@ export const deleteDeck = id => async dispatch => {
 export const createDeck = payload => async dispatch => {
   try {
     const response = await axiosWithAuth().post(`/decks`, payload);
-    let newDeck = {...response.data.deck};
-    newDeck.deck_name = newDeck.name;
-    delete newDeck.name;
-    console.log(newDeck);
     dispatch({
       type: CREATE_DECK,
-      payload: newDeck,
+      payload: response.data.deck,
     });
   } catch (err) {
     throw err;
